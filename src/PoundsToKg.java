e33258e940516282699242336f9b45d33d2096ae
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-08 11:17:42 -0400

    weeeeeee

--- src/PoundsToKg.java
+++ src/PoundsToKg.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class PoundsToKg {
+public class PoundsToKg {
+    public static void main(String[] args) {
+        double pounds = 200.0;
+        double kilograms = (pounds / 2.205);
+        System.out.println(kilograms);
+    }
 }
